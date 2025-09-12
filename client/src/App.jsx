954b5b7c86b4f5cdb8ca153eae5c5e0d4cceadf4
kunwarbhattal <partapbhattal@gmail.com>
2025-09-13 01:10:26 +0530

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = ()=>{
+  return (
+    <div className='bg-black'> 
+      <h2 className='text-white'>Hello There, My Friend</h2>
+    </div>
+  )
+}
+
+export default App
